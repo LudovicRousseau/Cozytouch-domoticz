b160d3a316a7e0a756bb5f1a64a953c9d0a8c2aa
Ludovic Rousseau <ludovic.rousseau@free.fr>
2022-12-12 18:53:11 +0100

    Dump server data in a .json file
    Since we dump JSON data we should use a .json extension.
    It help vim identify the correct syntax colorization.

--- cozytouch.py
+++ cozytouch.py
@@ -613,7 +613,7 @@ def decouverte_devices():
     data = cozytouch_GET('getSetup')
 
     if debug==2:
-	    f1=open('./dump_cozytouch.txt', 'w+')
+	    f1=open('./dump_cozytouch.json', 'w+')
 	    f1.write((json.dumps(data, indent=4, separators=(',', ': '))))
 	    f1.close()
 
